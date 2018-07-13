1c47c91fd4e90c4b8a6bf6d216d73674c5150cbf
Your Name <you@example.com>
2018-07-13 16:00:04 +0300

    Changed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -4,9 +4,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'jn-iqxcq330ier9-vul*fsa$k%h5*pjdulp5*a#qs8m*j!kj#r'
 
